7e688aa00cd3ad7a384789b823750f33649f74cb
胆汁酸 <1194964594@qq.com>
2025-07-08 17:04:26 +0800

    init: 初始化 TimeSnap 项目

---
+++ entry/src/main/ets/common/userStore.ts
@@ -0,0 +1,8 @@
+
+//实现登录后再修改
+
+export class UserStore {
+  static userId: string = '1';
+  static nickname: string = 'hyw';
+  static avatar: string = 'http://47.117.0.254:4000/uploads/avatar/1.png';
+}
